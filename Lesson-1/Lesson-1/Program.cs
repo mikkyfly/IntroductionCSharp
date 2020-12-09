745c6632f8345f7a317cc9d39c9548d85fab9759
MikkyFly <m.shikovskii@rosvodokanal.ru>
2020-12-09 09:38:31 +0500

    newcommit

---
+++ Lesson-1/Lesson-1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Доброго времени суток. Как тебя зовут мой друг?");
+            var name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}. Спрашиваешь сколько времени? {DateTime.Now}.");
+        }
+    }
+}
